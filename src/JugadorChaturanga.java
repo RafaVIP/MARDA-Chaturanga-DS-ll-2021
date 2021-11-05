77fd6eeed2ea73e6d7560705bd1dd7d963dd0828
wendy.ortizalfaro <wendy.ortizalfaro@ucr.ac.cr>
2021-11-04 19:55:35 -0600

    Correct Mistakes

--- src/JugadorChaturanga.java
+++ src/JugadorChaturanga.java
@@ -14,15 +14,6 @@ public class JugadorChaturanga extends JugadorAbstracto {
     pedirDatos(colorArray);
   }
 
-  public String to_String(){
-    String jugador = this.nombre + ", "+ this.color + ", [";
-    for (int piezaPerdida = 0; piezaPerdida < this.cantidadPiezasPerdidas; piezaPerdida++) {
-      jugador += "{" + piezasPerdidas.get(piezaPerdida).toString() + "}";
-    }
-    jugador += "\n";
-    return jugador;
-  }
-
   public void pedirDatos(ArrayList<String> colorArray) {
     this.nombre = JOptionPane.showInputDialog("Digite el nombre del jugador:");
     String[] colorArrayCopy = new String[colorArray.size()];
@@ -31,20 +22,20 @@ public class JugadorChaturanga extends JugadorAbstracto {
     }
     int option = JOptionPane.showOptionDialog(null, "Eliga un color para el jugador: ", 
       "Colores", 0, JOptionPane.QUESTION_MESSAGE, null, colorArrayCopy, "Blanco");
-    
-    if (option == 0) {
-      this.color = "blanco";
-    } else if (option == 1) {
-      this.color = "verde";
-    }else if (option == 2) {
-      this.color = "amarillo";
-    } else if (option == 3) {
-      this.color = "rojo";
-    }
+    this.color = colorArray.get(option);
 
   }
   public String getColor(){
     return this.color;
+  }
+
+  public String to_String(){
+    String jugador = this.nombre + ", "+ this.color + ", [";
+    for (int piezaPerdida = 0; piezaPerdida < this.cantidadPiezasPerdidas; piezaPerdida++) {
+      jugador += "{" + piezasPerdidas.get(piezaPerdida).toString() + "}";
+    }
+    jugador += "\n";
+    return jugador;
   }
 }
 
